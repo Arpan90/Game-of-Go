67a5915340a6d30979e60561deaead9b3439fd09
Arpan90 <51015806+Arpan90@users.noreply.github.com>
2019-08-01 23:31:33 +0530

    Moved functions & buttons to separate files & more
    1. Added facility to delete the stones considered dead before scoring the game. 
    2. Implemented scoring and added a scoring button.
    3. Blocks the player from making a move after scoring.

--- game.js
+++ game.js
@@ -1,36 +1,18 @@
-$('#btn1').click(function(){ // Pass button
-    if(pass === false){
-        if($('#move').html() === 'Black to move'){
-            $('#move').html('White to move');
-        }
-        else{
-            $('#move').html('Black to move');
-        }
-        pass = true;
-    }
-
-    else{
-        scoring(neighbourStones,groupNeighbours);
-    }
-    
-});
-
-$('#btn5').click(function(){ // Restart button
-    console.clear();
-    initializeMap();
-    // location.reload();
-});
-
 var currentColor;
-var pass = false;
+var pass;
 var reverseMode;
 var stateCount = 0;
-var ko = false;
+var ko;
 var rowCount = 0;
 var stoneCount = 0;
 var posMap;
 var states;
-$('.row').each(function(index,value){
+var whiteScore;
+var blackScore;
+var deadDeletion;
+var scored;
+
+$('.row').each(function(index,value){ // provides unique id to each row and to each stone in each row.
     $(value).attr('id', rowCount.toString());
     rowCount++;
     $(value).find('.stone').each(function(stoneIndex,stoneValue){
@@ -42,9 +24,15 @@ $('.row').each(function(index,value){
 
 initializeMap();
 
-$('.stone').click(function(){ if(reverseMode === false){//begin
+$('.grid').click(function(){
+    if(scored === true){
+        alert('Game Over!');
+    }
+});
+
+$('.stone').click(function(){ current = this; if(reverseMode === false && deadDeletion === false){ //begin
+    pass = false;
     // current turn
-    current = this;
     if($('#move').html() === 'Black to move'){
         currentColor = 'rgb(0, 0, 0)';
     }
@@ -119,8 +107,8 @@ $('.stone').click(function(){ if(reverseMode === false){//begin
             revertState();
         }
         else{
-            mapping(posMap,1);
-            stateDataupdate(states,posMap);
+            mapping(posMap,1); // save the changes on the game board to the positional map.
+            stateDataupdate(states,posMap); // save the current positional map in the latest key of the states object.
             
         }
         
@@ -128,231 +116,17 @@ $('.stone').click(function(){ if(reverseMode === false){//begin
 
     ko = false;
 
-}});// end
-
-
-
-$('#btn2').click(function(){ // Backward button
-    if(Object.keys(states).length-1 > stateCount){
-        stateCount += 1;
-        reverseMode = true;
-        var lastKey = Object.keys(states).length-1;
-        var key = lastKey-stateCount;
-        revertState(key);
-    }
-    
-});
-
-$('#btn3').click(function(){ // Forward button
-    if(reverseMode === true){ // Checking if the game has been reversed atall.
-        stateCount -= 1;
-        var lastKey = Object.keys(states).length-1;
-        var key = lastKey-stateCount;
-        revertState(key);
-    }
-    if(stateCount===0){
-        reverseMode=false;
-    }
-    
-});
-
-$('#btn4').click(function(){ // Resume button
-    reverseMode=false;
-    var lastKey = Object.keys(states).length-1;
-    var key = lastKey-stateCount;
-    stateCount = 0;
-    for(i in states){
-        if(i>key){
-            delete states[i];
-        }
-    }
-});
-
-/////  FUNCTIONS /////////////////////
-
-function logger(msg,obj){
-    return console.log(msg, JSON.parse(JSON.stringify(obj)));
 }
-
-
-function logger1(msg,obj){
-    return console.log(msg, obj);
-}
-
-function isKo(){
-    mapping(posMap,1);
-    stateDataupdate(states,posMap);
-    var lastKey = Object.keys(states).length-1;
-    for(let i in states){
-        if(i < lastKey){
-            if(processState(states[i]) === processState(states[lastKey]) && i%2===lastKey%2){ // modulus operator on key tells whether the key is even or odd which in turn indicates who's turn it was
-                alert('Ko! of '+lastKey+ ' with board position after move: '+i); 
-                delete states[lastKey];
-                posMap = JSON.parse(JSON.stringify(states[lastKey-1]));
-                return true;
-            }
-        }
-        
-    }
-    delete states[lastKey];
-    posMap = JSON.parse(JSON.stringify(states[lastKey-1]));
-    return false;
-}
-
-
-
-function groupNeighbours(classGrp,neighbourStones){
-    var nOc=[];
-    $('.'+classGrp).each(function(index,value){
-        let neighboursOfclass = neighbourStones(value);
-        for(let member of neighboursOfclass){
-            if($(member).attr('class').replace('stone ','') != classGrp){
-                nOc.push(member);
-            }
-        }
-    }); 
-    nOc = [...new Set(nOc)];
-    return nOc;
-}
-
-function calculateLiberties(classgrp){
-    var liberties = 0;
-    for(member of groupNeighbours(classgrp,neighbourStones)){
-        if($(member).css('background-color')==='rgba(0, 0, 0, 0)'){ 
-            liberties += 1;
-        }
-    }
-    return liberties;
-}
-
-
-function mapping(posMap,direction){
-    if(direction===1){ // grid to map
-        $('.stone').each(function(index,value){
-            rowNumber = mapLocation(value)[0];
-            stoneNumber = mapLocation(value)[1];
-            if($(value).css('background-color')==='rgb(0, 0, 0)'){
-                posMap[rowNumber][stoneNumber] = -Number($(value).attr('class').replace(/stone g/,''));
-            }
-
-            else if($(value).css('background-color')==='rgb(255, 255, 255)'){
-                posMap[rowNumber][stoneNumber] = Number($(value).attr('class').replace(/stone g/,''));
-            }
-
-            else{
-                posMap[rowNumber][stoneNumber] = 0;
-            }
-        });
-    }
-
-    else{
-        $('.stone').each(function(index,value){ // map to grid
-            rowNumber = mapLocation(value)[0];
-            stoneNumber = mapLocation(value)[1];
-            if(posMap[rowNumber][stoneNumber] === 0){
-                $(value).css('background-color','rgba(0, 0, 0, 0)');
-                $(value).attr('class','stone initial');
-            }
-
-            else if(posMap[rowNumber][stoneNumber] > 0){
-                $(value).css('background-color','rgb(255, 255, 255)');
-                $(value).attr('class','stone g' + (posMap[rowNumber][stoneNumber]).toString());
-            }
-
-            else{
-                $(value).css('background-color','rgb(0, 0, 0)');
-                $(value).attr('class','stone g' + (-posMap[rowNumber][stoneNumber]).toString());
-            }
-        });
+else if(deadDeletion === true){ // tells whether both players have passed their moves consecutively or not.
+    if($(current).css('background-color') != 'rgba(0, 0, 0, 0)'){ 
+        $(current).css('background-color','rgba(0, 0, 0, 0)') // deletes the stone chosen as dead.
+        posMap[mapLocation(current)[0]] [mapLocation(current)[1]] = 0;
+        stateDataupdate(states,posMap);
     }
 }
+});// end
 
-function mapLocation(node){
-    rowNumber = Number($(node).closest('.row').attr('id'));
-    stoneNumber = Number($(node).attr('id'));
-    return [rowNumber, stoneNumber];
-}
 
-function gridLocation(i,j){
-    var result;
-    $('.stone').each(function(index,value){
-        rowNumber = Number($(value).closest('.row').attr('id'));
-        stoneNumber = Number($(value).attr('id'));
-        if(i===rowNumber && j===stoneNumber){
-            result = value;
-            return false; // to exit $.each() loop
-        }
-    });
-    return result;
-}
-
-function stateDataupdate(states,posMap){
-    states[Object.keys(states).length] = [];
-    states[Object.keys(states).length-1]=JSON.parse(JSON.stringify(posMap));
-}
-
-function initializeMap(){
-    posMap = [];
-    for(let i = 0 ; i < 19 ; i++){
-        posMap[i]=[]
-        for(let j = 0 ; j < 19 ; j++){
-            posMap[i].push(0);
-        }
-    } 
-
-    states={0:[]};
-    states[0]=JSON.parse(JSON.stringify(posMap));
-    states[0].push({});
-
-    reverseMode = false;
-    $('.stone').css('background-color','rgba(0, 0, 0, 0)');
-    $('.stone').attr('class','stone initial');
-    $('#move').html('Black to move');
-
-}
-
-function neighbourStones(node){
-    var nodeOnmap = mapLocation(node);
-    var neighbours=[];
-    neighbours[0] = gridLocation(nodeOnmap[0]-1, nodeOnmap[1]);
-    neighbours[1] = gridLocation(nodeOnmap[0], nodeOnmap[1]+1);
-    neighbours[2] = gridLocation(nodeOnmap[0]+1, nodeOnmap[1]);
-    neighbours[3] = gridLocation(nodeOnmap[0], nodeOnmap[1]-1);
-
-    for(let neighbour of neighbours){
-        if(typeof($(neighbour).attr('class'))==='undefined'){
-            neighbours.splice(neighbours.indexOf(neighbour),1);
-        }
-    }
-    return neighbours;
-}
-
-function revertState(key=Object.keys(states).length-1){
-    mapping(states[key],-1);
-    if(key%2===0){
-        $('#move').html('Black to move');
-    }
-
-    else{
-        $('#move').html('White to move');
-    }
-}
-
-function processState(map){
-    a = JSON.parse(JSON.stringify(map));
-    for(let i = 0; i < 19; i++){
-        for(let j = 0; j < 19; j++){
-            if(a[i][j] < 0){
-                a[i][j] = -1;
-            }
-            else if(a[i][j] > 0){
-                a[i][j] = 1;
-            }
-        }
-    }
-    delete a[19];
-    return JSON.stringify(a);
-}
 
 
 
